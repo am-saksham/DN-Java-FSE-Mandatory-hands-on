671e183c5c80c316b71291759872ee4ae6c61e24
Saksham <saksham.23bce10006@vitbhopal.ac.in>
2026-07-11 10:28:38 +0530

    Added Spring REST: Get country based on PathVariable using Service layer

--- Week-3/spring-rest-handson/hello-world-rest/src/main/java/com/cognizant/springlearn/controller/CountryController.java
+++ Week-3/spring-rest-handson/hello-world-rest/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -2,30 +2,46 @@ package com.cognizant.springlearn.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cognizant.springlearn.Country;
+import com.cognizant.springlearn.service.CountryService;
 
 @RestController
 public class CountryController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CountryController.class);
 
+    // Spring automatically injects the CountryService here
+    @Autowired
+    private CountryService countryService;
+
+    // Previous Exercise (Hardcoded to IN)
     @RequestMapping(value = "/country", method = RequestMethod.GET)
     public Country getCountryIndia() {
         LOGGER.info("Start getCountryIndia");
-        
-        // Load the bean from the XML configuration
         ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
-        Country country = context.getBean("country", Country.class);
-        
+        Country country = context.getBean("in", Country.class); // updated to match new bean id
         LOGGER.info("End getCountryIndia");
-        
-        // Return the Java object directly!
         return country;
+    }
+
+    // New Exercise (Dynamic based on PathVariable)
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+        LOGGER.info("Start getCountry (Dynamic Code)");
+        
+        // Pass the request off to the service layer
+        Country matchedCountry = countryService.getCountry(code);
+        
+        LOGGER.info("End getCountry (Dynamic Code)");
+        return matchedCountry;
     }
 }
